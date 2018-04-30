e3f7dc182bbf86090f1531230e2735f87630fd23
Ibrar <30756862+ibrarnaseer@users.noreply.github.com>
2018-04-30 13:00:16 +0200

    Update drop_table.js

--- drop_table.js
+++ drop_table.js
@@ -10,7 +10,8 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   var sql = "DROP TABLE IF EXISTS customers";
-  // Slette tabell kun hvis den eksisterer (for å ikke få feilmeldinger). 
+  /* Slette tabell kun hvis den eksisterer (for å ikke få feilmeldinger). 
+  Fjerne if statement for å bare slette uansett. */
   con.query(sql, function(err, result) {
     if (err) throw err;
     console.log("Table deleted.");
