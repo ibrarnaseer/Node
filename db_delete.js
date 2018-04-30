13eabadd6d170865672fa0bf44fda2622b793d49
Ibrar <30756862+ibrarnaseer@users.noreply.github.com>
2018-04-30 12:58:04 +0200

    Delete from db
    For å slette innhold i tabeller.

---
+++ db_delete.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "********",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  var sql = "DELETE FROM customers WHERE address = 'Highway 37'";
+  // Slette alle kunder som har adressen 'Highway 37'. 
+  con.query(sql, function(err, result) {
+    if (err) throw err;
+    console.log("Number of records deleted: " + result.affectedRows);
+  });
+});
